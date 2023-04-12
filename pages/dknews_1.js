b73e4c62cfdd474c685b3573d4e8e7f892b1b589
DESKTOP-VC2DAST\bsnmj <bsnmjr@gmail.com>
2023-04-12 22:16:11 +0900

    news jpg 를 서버에서 가져오기

--- pages/dknews_1.js
+++ pages/dknews_1.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import Footer from '../components/footer';
 import Navbar from '../components/navbar';
+import { API_URL } from '../config';
 
 const _dknews = () => {
   const router = useRouter();
@@ -17,7 +18,7 @@ const _dknews = () => {
       <Navbar />
       <div className='main'>
             <div>
-              <img src='/img/dknews/dknews1.jpg' width="100%"></img>
+{/*               <img src='/img/dknews/dknews1.jpg' width="100%"></img>
               <img src='/img/dknews/dknews2.jpg' width="100%"></img>
               <img src='/img/dknews/dknews3.jpg' width="100%"></img>
               <img src='/img/dknews/dknews4.jpg' width="100%"></img>
@@ -25,6 +26,16 @@ const _dknews = () => {
               <img src='/img/dknews/dknews6.jpg' width="100%"></img>
               <img src='/img/dknews/dknews7.jpg' width="100%"></img>
               <img src='/img/dknews/dknews8.jpg' width="100%"></img>
+ */} 
+              <img src='API_URL/dknews_jpg/dknews1.jpg' width="100%"></img>
+              <img src='API_URL/dknews_jpg/dknews2.jpg' width="100%"></img>
+              <img src='API_URL/dknews_jpg/dknews3.jpg' width="100%"></img>
+              <img src='API_URL/dknews_jpg/dknews4.jpg' width="100%"></img>
+              <img src='API_URL/dknews_jpg/dknews5.jpg' width="100%"></img>
+              <img src='API_URL/dknews_jpg/dknews6.jpg' width="100%"></img>
+              <img src='API_URL/dknews_jpg/dknews7.jpg' width="100%"></img>
+              <img src='API_URL/dknews_jpg/dknews8.jpg' width="100%"></img>
+
             </div>
       </div>
       <Footer />
